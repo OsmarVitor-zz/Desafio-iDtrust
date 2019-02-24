e227679a060a1b627486b92e1850bff8af000c13
Vitor Perez <38172165+OsmarVitor@users.noreply.github.com>
2019-02-24 19:37:35 -0300

    Update Book.java

--- iDtrust/src/main/java/com/mycompany/desafio/Book.java
+++ iDtrust/src/main/java/com/mycompany/desafio/Book.java
@@ -8,47 +8,59 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="TB_BOOK")
-public class Book {
-    
-    @Column
+@Table(name = "TB_BOOK")
+public class Book{
+
+    @Column(name = "id")
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @Column
+    @Column(name = "nameBook")
     private String nameBook;
 
-    @Column
+    @Column(name = "autoBook")
     private String authorBook;
 
-    public Book(long id, String nameBook, String authorBook) {
+    @Column(name = "isbn")
+    private String isbn;
+
+    public Book(long id, String nameBook, String authorBook, String isbn) {
+        this.isbn = isbn;
         this.id = id;
         this.nameBook = nameBook;
         this.authorBook = authorBook;
     }
-
+    
     public long getId() {
         return id;
     }
-
+    
     public String getNameBook() {
         return nameBook;
     }
-
+    
     public String getAuthorBook() {
         return authorBook;
+    }
+    
+    public String getIsbn() {
+        return isbn;
     }
 
     public void setId(long id) {
         this.id = id;
     }
-
+    
     public void setNameBook(String nameBook) {
         this.nameBook = nameBook;
     }
-
+    
     public void setAuthorBook(String authorBook) {
         this.authorBook = authorBook;
+    }
+    
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 }
